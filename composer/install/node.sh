2f6bd6180b503961b273c7ab7b7934322c9f1d05
Percival Lucena <plucena@gmail.com>
2017-06-10 23:59:19 -0300

    Create node.sh

---
+++ composer/install/node.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+
+sudo apt-get -y install build-essential libssl-dev python-pip git
+
+curl -sL https://deb.nodesource.com/setup_6.x > node-install
+
+sudo chmod 777 node-install
+
+sudo ./node-install
+
+sudo apt-get install -y nodejs
+
