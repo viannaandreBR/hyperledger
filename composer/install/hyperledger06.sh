a5595038fbff1bfdb765de971e52e3ee74a26782
Percival Lucena <plucena@gmail.com>
2017-06-10 23:47:54 -0300

    Create hyperledger06.sh

---
+++ composer/install/hyperledger06.sh
@@ -0,0 +1,8 @@
+wget -qO- https://get.docker.com/ | sh
+
+sudo pip install docker-compose
+
+git clone https://github.com/fabric-composer/sample-applications
+
+/root/sample-applications/packages/getting-started/scripts/setup.sh
+
